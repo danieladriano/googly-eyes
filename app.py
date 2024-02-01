6ca4d3a9b78077c55272c0479b574d42d995fec6
Daniel Adriano <daniel.dadriano@gmail.com>
2024-02-01 21:43:28 +0000

    Detect eyes and randomize googly eyes drawing

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 """Flask app to detect faces in an image and add googly eyes to them."""
 
+import logging
+import random
+
 import cv2
 import numpy as np
 from flask import Flask, make_response, request
 
 app = Flask(__name__)
 face_cascade = cv2.CascadeClassifier("./model/haarcascade_frontalface_default.xml")
+eyes_cascade = cv2.CascadeClassifier("./model/haarcascade_eye.xml")
 
 
 def detect_faces(gray_image: np.ndarray) -> np.ndarray:
@@ -22,6 +26,55 @@ def detect_faces(gray_image: np.ndarray) -> np.ndarray:
     )
 
 
+def detect_eyes(gray_image: np.ndarray) -> np.ndarray:
+    """Detect eyes in a gray image.
+
+    Args:
+        gray_image (np.ndarray): A gray image to detect eyes.
+
+    Returns:
+        np.ndarray: An array of eyes (x, y, w, h)
+    """
+    return eyes_cascade.detectMultiScale(
+        image=gray_image, scaleFactor=1.2, minNeighbors=5
+    )
+
+
+def draw_googly_eyes(image: np.ndarray, eyes: np.ndarray) -> np.ndarray:
+    """Draw googly eyes in an image.
+
+    Args:
+        image (np.ndarray): image to draw googly eyes.
+        eyes (np.ndarray): eyes coordinates.
+
+    Returns:
+        np.ndarray: image with googly eyes.
+    """
+    image_h = np.size(image, 0)
+    for x, y, w, h in eyes:
+        try:
+            if y + h > image_h / 2:
+                pass
+            center = (int(x + w / 2), int(y + h / 2))
+            radius = int(w * (0.7 + random.random()))
+            cv2.circle(
+                img=image,
+                center=center,
+                radius=radius,
+                color=(255, 255, 255),
+                thickness=-1,
+            )
+            cv2.circle(
+                img=image,
+                center=center,
+                radius=int(radius / 2),
+                color=(0, 0, 0),
+                thickness=-1,
+            )
+        except Exception as ex:
+            logging.error("Error drawing eyes: %s", ex)
+
+
 def get_image_with_faces(image: np.ndarray) -> np.ndarray:
     """Return the input image with faces detected.
 
@@ -31,11 +84,13 @@ def get_image_with_faces(image: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: image with faces detected.
     """
-    faces = detect_faces(gray_image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = detect_faces(gray_image=gray_image)
     for x, y, w, h in faces:
-        cv2.rectangle(
-            img=image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=3
-        )
+        roi_gray_image = gray_image[y : y + h, x : x + w]
+        roi_image = image[y : y + h, x : x + w]
+        eyes = detect_eyes(gray_image=roi_gray_image)
+        draw_googly_eyes(image=roi_image, eyes=eyes)
     return image
 
 
